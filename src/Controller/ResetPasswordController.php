f15042406b5c77bdbe435c30534e482e1047ed84
marlene <marlene.mlg53@gmail.com>
2024-02-22 19:52:32 +0100

    Add message reset password

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -121,6 +121,11 @@ class ResetPasswordController extends AbstractController
             // The session is cleaned up after the password has been changed.
             $this->cleanSessionAfterReset();
 
+            $this->addFlash(
+                'success',
+                'Your password is reset'
+            );
+
             return $this->redirectToRoute('home');
         }
 
@@ -163,8 +168,7 @@ class ResetPasswordController extends AbstractController
             ->htmlTemplate('reset_password/email.html.twig')
             ->context([
                 'resetToken' => $resetToken,
-            ])
-        ;
+            ]);
 
         $mailer->send($email);
 
